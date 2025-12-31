a3a2fa5a8b63797a92d29edf0fb0dd9a47848fbe
Karan Gandha <123099412+karanxlab@users.noreply.github.com>
2025-12-31 14:54:47 +0530

    Update fetch.v

--- fetch.v
+++ fetch.v
@@ -1,27 +1,27 @@
 module fetch (
     input clk, reset,
     input [31:0] pc_next,
-    input stall, flush, 
-    // stall : A control signal to pause the stage, preventing PC or instruction updates
-    // flush: A control signal to clear the stage, resetting the PC
-    output reg [31:0] pc, //The current Program Counter value, used to address memory
-    output reg [31:0] instr,// The fetched 32-bit instruction from memory
+    input stall, flush,
+    output reg [31:0] pc,
+    output reg [31:0] instr,
     output [31:0] pc_out,
-    input [31:0] mem_data // 32-bit memory data
+    input [31:0] mem_data
 );
-    // PC Register
+
     always @(posedge clk or posedge reset) begin
-        if (reset) pc <= 0;
-        else if (flush) pc <= 0; // Reset PC on flush 
-        else if (!stall) pc <= pc_next; // Update PC unless stalled
+        if (reset)
+            pc <= 0;
+        else if (flush)
+            pc <= 0;
+        else if (!stall)
+            pc <= pc_next;
     end
 
-    // Instruction Fetch
     always @(posedge clk) begin
-        if (!stall && !flush) instr <= mem_data; // Fetch instruction from memory
+        if (!stall && !flush)
+            instr <= mem_data;
     end
 
-    // Output PC for memory addressing
     assign pc_out = pc;
 
 endmodule
