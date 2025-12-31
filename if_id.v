fcf5de117d7a72e0d8f0c56bba695939adfbe108
Karan Gandha <123099412+karanxlab@users.noreply.github.com>
2025-12-31 14:57:41 +0530

    Update if_id.v

--- if_id.v
+++ if_id.v
@@ -6,6 +6,7 @@ module if_id(
     output reg [31:0] pc_out,
     output reg [31:0] instr_out
 );
+
     always @(posedge clk or posedge reset) begin
         if (reset || flush) begin
             pc_out <= 0;
@@ -15,4 +16,5 @@ module if_id(
             instr_out <= instr_in;
         end
     end
+
 endmodule
